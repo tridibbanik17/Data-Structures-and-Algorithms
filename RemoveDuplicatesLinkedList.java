75917cd89d0c4490dc025510ab557fb0d60a921d
Tridib Banik <tridib.perfect@gmail.com>
2025-05-03 23:06:03 -0400

    remove duplicates from an unsorted linked list.

---
+++ RemoveDuplicatesLinkedList.java
@@ -0,0 +1,62 @@
+// remove duplicates from an unsorted linked list.
+public class RemoveDuplicatesLinkedList {
+    public static class Node {
+        Node next;
+        int data;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public static void remove(Node head) {
+        if (head == null) return;
+
+        Node current = head;
+
+        while (current != null) {
+            Node nextNode = current;
+
+            while (nextNode.next != null) {
+                if (current.data == nextNode.next.data) {
+                    nextNode.next = nextNode.next.next;
+                } else {
+                    nextNode = nextNode.next;
+                }
+            }
+
+            current = current.next;
+        }
+    }
+
+    public static void print(Node head) {
+        Node current = head;
+
+        while (current != null) {
+            System.out.println(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(1);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = new Node(4);
+        head.next.next.next.next.next = new Node(4);
+        head.next.next.next.next.next.next = new Node(4);
+        head.next.next.next.next.next.next.next = new Node(5);
+        head.next.next.next.next.next.next.next.next = new Node(5);
+
+        System.out.println("Original List: ");
+        print(head);
+
+        remove(head);
+        System.out.println("Modified list after removing duplicates: ");
+        print(head);
+    }
+}
