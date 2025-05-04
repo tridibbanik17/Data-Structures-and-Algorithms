478af37b4ddc9047214936d4307c948e151e6540
Tridib Banik <tridib.perfect@gmail.com>
2025-05-04 00:25:39 -0400

    remove duplicates from an unsorted linked list using a HashSet

--- RemoveDuplicatesLinkedListUsingHashSet.java
+++ RemoveDuplicatesLinkedListUsingHashSet.java
@@ -34,7 +34,7 @@ public class RemoveDuplicatesLinkedListUsingHashSet {
         Node current = head;
 
         while (current != null) {
-            System.out.println(current.data + " ");
+            System.out.print(current.data + " ");
             current = current.next;
         }
         System.out.println();
